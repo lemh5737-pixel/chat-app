fe31222ca50633eb2c3639445c80969633ca716e
lemh5737-pixel <lemh5737@gmail.com>
2025-09-29 17:12:38 +0700

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -672,6 +672,20 @@ export default function ChatPage() {
             </button>
             
             <button
+              onClick={() => router.push('/stories')}
+              className={`flex flex-col items-center justify-center py-3 px-6 ${
+                router.pathname === '/stories'
+                  ? 'text-indigo-600 dark:text-indigo-400'
+                  : 'text-gray-500 dark:text-gray-400'
+              }`}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
+              <span className="text-xs mt-1">Stories</span>
+            </button>
+            
+            <button
               onClick={() => router.push('/')}
               className={`flex flex-col items-center justify-center py-3 px-6 ${
                 router.pathname === '/'
