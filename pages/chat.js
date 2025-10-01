0f7aebe79475712f7a9cf6e5998a7e687ad0724a
lemh5737-pixel <lemh5737@gmail.com>
2025-10-01 17:35:54 +0700

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -685,6 +685,21 @@ export default function ChatPage() {
               <span className="text-xs mt-1">Contacts</span>
             </button>
             
+            {/* Tombol navigasi ke halaman komunitas */}
+            <button
+              onClick={() => router.push('/community')}
+              className={`flex flex-col items-center justify-center py-3 px-6 ${
+                router.pathname === '/community'
+                  ? 'text-indigo-600 dark:text-indigo-400'
+                  : 'text-gray-500 dark:text-gray-400'
+              }`}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656-.126-1.283-.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="text-xs mt-1">Community</span>
+            </button>
+            
             <button
               onClick={() => router.push('/profile')}
               className={`flex flex-col items-center justify-center py-3 px-6 ${
